3eabb782537c12fc9a35cd8280c29a9b0ffeb8ad
Stefan Mihajlovski <smihajlovski89@gmail.com>
2016-05-04 17:06:59 +0200

    Trying to JSON

--- app/src/main/java/com/example/digital05/jsonexample/MainActivity.java
+++ app/src/main/java/com/example/digital05/jsonexample/MainActivity.java
@@ -3,9 +3,19 @@ package com.example.digital05.jsonexample;
 import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.digital05.jsonexample.app.AppController;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,20 +59,53 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void makeJsonObjectRequest(){
+    private void makeJsonObjectRequest() {
+        showpDialog();
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                urlJsonObj, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, response.toString());
+
+                try {
+                    String name = response.getString("name");
+                    String email = response.getString("email");
+                    JSONObject phone = response.getJSONObject("phone");
+                    String home = response.getString("home");
+                    String mobile = response.getString("mobile");
+
+                    jsonResponse = "";
+                    jsonResponse += "Name " + name + "\n\n";
+                    jsonResponse += "Email " + email + "\n\n";
+                    jsonResponse += "Phone " + phone + "\n\n";
+                    jsonResponse += "Home " + home + "\n\n";
+                    jsonResponse += "Mobile " + mobile + "\n\n";
+
+                    txtResponse.setText(jsonResponse);
+                } catch (JSONException e){
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                hidepDialog();
+            }, new Response.ErrorListener(){
+
+            }
+        });
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
 
     }
 
-    private void makeJsonArrayRequest(){
+    private void makeJsonArrayRequest() {
 
     }
 
-    private void showpDialog(){
+    private void showpDialog() {
         if (!pDialog.isShowing())
             pDialog.show();
     }
 
-    private void hidepDialog(){
+    private void hidepDialog() {
         if (pDialog.isShowing())
             pDialog.dismiss();
     }
